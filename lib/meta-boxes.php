cef52b995e086c8ee9fb9c84b0a0f869ba97a65d
mikey ray-von <mikey@interglobal.vision>
2016-08-15 17:44:09 -0700

    add image gallery metaboxes

--- lib/meta-boxes.php
+++ lib/meta-boxes.php
@@ -39,5 +39,60 @@ function igv_cmb_metaboxes() {
    * Reference: https://github.com/WebDevStudios/CMB2/blob/master/example-functions.php
 	 */
 
+  $image_gallery_group = new_cmb2_box( array(
+    'id'           => $prefix . 'image_gallery_metabox',
+    'title'        => __( 'Image gallery', 'cmb2' ),
+    'object_types' => array( 'post', ),
+  ) );
+
+  // $group_field_id is the field id string, so in this case: $prefix . 'demo'
+  $image_gallery_field_id = $image_gallery_group->add_field( array(
+    'id'          => $prefix . 'image_gallery',
+    'type'        => 'group',
+    'description' => __( '', 'cmb2' ),
+    'options'     => array(
+      'group_title'   => __( 'Image {#}', 'cmb2' ), // {#} gets replaced by row number
+      'add_button'    => __( 'Add Another Image', 'cmb2' ),
+      'remove_button' => __( 'Remove Image', 'cmb2' ),
+      'sortable'      => true, // beta
+      // 'closed'     => true, // true to have the groups closed by default
+    ),
+  ) );
+
+  $image_gallery_group->add_group_field( $image_gallery_field_id, array(
+    'name' => __( 'Image File', 'cmb2' ),
+    'description' => __( '(Suggested min width 1150px)', 'cmb2' ),
+    'id'   => 'image',
+    'type' => 'file',
+  ) );
+
+  $image_gallery_group->add_group_field( $image_gallery_field_id, array(
+    'name' => __( 'Image Caption', 'cmb2' ),
+    'description' => __( '(Suggested max 20 words)', 'cmb2' ),
+    'id'   => 'caption',
+    'type' => 'text',
+  ) );
+
+  $image_gallery_group->add_group_field( $image_gallery_field_id, array(
+    'name'        => __( 'Single image row', 'cmb2' ),
+    'description' => __( '(Centered image in full-width column)', 'cmb2' ),
+    'id'          => 'single_row',
+    'type'        => 'checkbox',
+  ) );
+
+  $image_gallery_group->add_group_field( $image_gallery_field_id, array(
+    'name'        => __( 'Percent width', 'cmb2' ),
+    'description' => __( '% (Percent width in column / Default 100 / Max 100)', 'cmb2' ),
+    'id'          => 'percent_width',
+    'type'        => 'text_small',
+  ) );
+
+  $image_gallery_group->add_group_field( $image_gallery_field_id, array(
+    'name'        => __( 'Percent rotate', 'cmb2' ),
+    'description' => __( '% (Clockwise: # / Counter-clockwise: -# / Default 0 / Max 100)', 'cmb2' ),
+    'id'          => 'percent_rotate',
+    'type'        => 'text_small',
+  ) );
+
 }
 ?>
