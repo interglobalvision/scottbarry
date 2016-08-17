07147b7b7d03d78288046a39060b5344cd03ea5a
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-08-17 16:33:27 -0700

    add single template

---
+++ single.php
@@ -0,0 +1,79 @@
+<?php
+get_header();
+?>
+
+<!-- main content -->
+
+<main id="main-content" class="margin-bottom-large">
+
+  <!-- main posts loop -->
+  <section id="post" class="container">
+
+<?php
+if( have_posts() ) {
+  while( have_posts() ) {
+    the_post();
+
+    $images = get_post_meta($post->ID, '_igv_image_gallery', true);
+
+    //pr($images[0][0]['image_id']); die;
+?>
+
+      <article <?php post_class('row'); ?>>
+<?php
+    if (get_the_content()) {
+?>
+        <div class="col col-s-12"><?php the_content(); ?></div>
+<?php 
+    }
+
+    foreach ($images as $image) {
+
+      $image_id = $image['image_id'];
+      $single_row = empty($image['single_row']) ? '' : $image['single_row'];
+      $top_margin = empty($image['top_margin']) ? '0' : $image['top_margin'];
+      $percent_width = empty($image['percent_width']) ? '100' : $image['percent_width'];
+      $degrees_rotate = empty($image['degrees_rotate']) ? '0' : $image['degrees_rotate'];
+
+      $image_size = $single_row == 'on' ? 'col-12' : 'col-6'
+?>
+        <div class="<?php 
+          if ($single_row == 'on') {
+            echo 'text-align-center col col-s-12'; 
+          } else {
+            echo 'text-align-center col col-s-12 col-m-6';
+          }
+        ?>" style="margin-top: <?php echo $top_margin; ?>px">
+
+          <?php echo wp_get_attachment_image($image_id, $image_size, false, array('style'=>'max-width: ' . $percent_width . '%; transform: rotate(' . $degrees_rotate . 'deg)')); ?>
+<?php 
+      if (!empty($image['caption'])) {
+?>
+          <div class="font-caption text-align-center margin-top-small"><?php echo $image['caption'] ?></div>
+<?php 
+      }
+?>
+
+        </div>
+
+<?php 
+    }
+?>
+      </article>
+
+<?php
+  }
+} 
+?>
+  <!-- end posts -->
+  </section>
+
+  <?php get_template_part('partials/pagination'); ?>
+
+<!-- end main-content -->
+
+</main>
+
+<?php
+get_footer();
+?>
