8075e448c75eea7377e46aa58c521bd539ef58a1
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-08-18 19:51:14 -0700

    remove loop. fix col offset. fix apply_filters

--- page.php
+++ page.php
@@ -1,5 +1,7 @@
 <?php
 get_header();
+
+$content_two = get_post_meta($post->ID, '_igv_content_two', true);
 ?>
 
 <!-- main content -->
@@ -9,32 +11,19 @@ get_header();
   <!-- main posts loop -->
   <section id="page" class="container">
 
-<?php
-if( have_posts() ) {
-  while( have_posts() ) {
-    the_post();
+    <article <?php post_class('row font-size-mid font-serif'); ?>>
+      <div class="col col-s-12 col-m-6 col-l-5 offset-l-1">
+        <?php the_content(); ?>
+      </div>
+      <div class="col col-s-12 col-m-6 col-l-5">
+        <?php 
+          if (!empty($content_two)) {
+            echo apply_filters('the_content', $content_two);
+          }
+        ?>
+      </div>
+    </article>
 
-    $content_two = get_post_meta($post->ID, '_igv_content_two', true);
-?>
-
-      <article <?php post_class('row font-size-mid font-serif'); ?>>
-        <div class="col col-s-12 col-m-6 col-l-4 offset-l-2">
-          <?php the_content(); ?>
-        </div>
-        <div class="col col-s-12 col-m-6 col-l-4 offset-l-2">
-          <?php 
-            if (!empty($content_two)) {
-              echo apply_filter('the_content', $content_two);
-            }
-          ?>
-        </div>
-      </article>
-
-
-<?php
-  }
-}
-?>
   <!-- end posts -->
   </section>
 
