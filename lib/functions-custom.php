12d93581080ab89543e28a91eebbe767da67477c
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-08-22 19:22:05 -0700

    add function to get post options for select

--- lib/functions-custom.php
+++ lib/functions-custom.php
@@ -1,3 +1,18 @@
 <?php
 
 // Custom functions (like special queries, etc)
+
+function cmb2_get_post_options($args) {
+
+  $posts = get_posts($args);
+
+  $post_options = array();
+
+  if ($posts) {
+    foreach ($posts as $post) {
+      $post_options[$post->ID] = $post->post_title;
+    }
+  }
+
+  return $post_options;
+ }
