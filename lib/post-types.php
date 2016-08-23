a644c3d5c69c13d1abefcb8b3659bbf1a1b0dc43
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-08-22 19:12:57 -0700

    add project post type

--- lib/post-types.php
+++ lib/post-types.php
@@ -10,6 +10,9 @@ function add_menu_icons_styles(){
 #menu-posts-conversation .dashicons-admin-post:before {
   content: '\f125';
 }
+#menu-posts-project .dashicons-admin-post:before {
+  content: "\f232";
+}
 </style>
 
 <?php
@@ -19,9 +22,51 @@ add_action( 'admin_head', 'add_menu_icons_styles' );
 
 //Register Custom Post Types
 
-add_action( 'init', 'register_cpt_conversation' );
+add_action( 'init', 'register_cpt_igv' );
 
-function register_cpt_conversation() {
+function register_cpt_igv() {
+
+// Projects
+  
+  $labels = array( 
+    'name' => _x( 'Projects', 'project' ),
+    'singular_name' => _x( 'Project', 'project' ),
+    'add_new' => _x( 'Add New', 'project' ),
+    'add_new_item' => _x( 'Add New Project', 'project' ),
+    'edit_item' => _x( 'Edit Project', 'project' ),
+    'new_item' => _x( 'New Project', 'project' ),
+    'view_item' => _x( 'View Project', 'project' ),
+    'search_items' => _x( 'Search Projects', 'project' ),
+    'not_found' => _x( 'No project found', 'project' ),
+    'not_found_in_trash' => _x( 'No project found in Trash', 'project' ),
+    'parent_item_colon' => _x( 'Parent Project:', 'project' ),
+    'menu_name' => _x( 'Projects', 'project' ),
+  );
+
+  $args = array( 
+    'labels' => $labels,
+    'hierarchical' => false,
+    
+    'supports' => array( 'title', 'editor' ),
+    
+    'public' => true,
+    'show_ui' => true,
+    'show_in_menu' => true,
+    'menu_position' => 5,
+    
+    'show_in_nav_menus' => true,
+    'publicly_queryable' => true,
+    'exclude_from_search' => false,
+    'has_archive' => true,
+    'query_var' => true,
+    'can_export' => true,
+    'rewrite' => true,
+    'capability_type' => 'post'
+  );
+
+  register_post_type( 'project', $args );
+
+// Conversations
 
   $labels = array( 
     'name' => _x( 'Conversations', 'conversation' ),
@@ -60,4 +105,5 @@ function register_cpt_conversation() {
   );
 
   register_post_type( 'conversation', $args );
+
 }
