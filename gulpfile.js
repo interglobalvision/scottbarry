d8515597458fc5cc0ce0ed88e974f32bb0ac6def
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2016-10-07 13:44:46 -0500

    support last 5 browser versions

--- gulpfile.js
+++ gulpfile.js
@@ -61,7 +61,9 @@ gulp.task('style', function() {
       ],
     }))
   .on('error', errorNotify)
-  .pipe(autoprefixer())
+  .pipe(autoprefixer({
+    browsers: ['last 5 versions'],
+  }))
   .on('error', errorNotify)
   .pipe(gulp.dest('css'))
   .pipe(rename({suffix: '.min'}))
