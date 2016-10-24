041598f8f6b63813929fc6b51fa131da15c35da3
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2016-10-24 18:56:04 -0500

    add artificial italic in sub-menus

--- js/main.js
+++ js/main.js
@@ -50,6 +50,8 @@ Site.Header = {
     if ($('.menu-trigger').length) {
       _this.bindMenuToggles();
     }
+
+    _this.hoverSkew();
   },
 
   bindMenuToggles: function() {
@@ -101,6 +103,24 @@ Site.Header = {
       _this.$header.removeClass('projects-open sort-open');
       _this.$header.addClass('open ' + menuName + '-open');
     } 
+  },
+
+  hoverSkew: function() {
+    var characters;
+
+    if ($('.hover-skew').length) {
+      $('.hover-skew').each(function() {
+        var $this = $(this);
+
+        characters = $this.text().split("");
+
+        $this.empty();
+        $.each(characters, function (i, el) {
+          var spaceless = el.replace(/\s+/g, '&nbsp;');
+          $this.append("<span>" + spaceless + "</span>");
+        });
+      });
+    }
   }
 };
 
