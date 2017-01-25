a43e106fd7c8d3522cc825c53cdf9ef08e56255c
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-01-25 11:47:40 -0600

    add footer circle

--- footer.php
+++ footer.php
@@ -1,4 +1,11 @@
     <footer id="footer">
+      <div class="container">
+        <div class="row padding-bottom-small">
+          <div class="col col-s-12 col-no-margin-bottom">
+            <svg id="circle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 28"><circle cx="14" cy="14" r="10"/></svg>
+          </div>
+        </div>
+      </div>
     </footer>
 
   </section>
