70b90fe8acb2824ce2df0774bf6976f77f875a8d
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-01-25 10:00:33 -0600

    add image size for list images

--- lib/thumbnail-sizes.php
+++ lib/thumbnail-sizes.php
@@ -8,6 +8,8 @@ if( function_exists( 'add_image_size' ) ) {
   add_image_size( 'admin-thumb', 150, 150, false );
   add_image_size( 'opengraph', 1200, 630, true );
 
+  add_image_size( 'list', 200, 9999, false );
+
   add_image_size( 'col-6', 1170, 9999, false );
   add_image_size( 'col-12', 2370, 9999, false );
 }
